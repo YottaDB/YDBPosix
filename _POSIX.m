0b9869c33e63081624275bbc2b8e8f74ef139229
Narayanan Iyer <nars@yottadb.com>
2018-10-16 17:56:59 -0400

    Add YottaDB copyrights

--- _POSIX.m
+++ _POSIX.m
@@ -1,14 +1,17 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;									;
-; Copyright (c) 2012-2015 Fidelity National Information 	;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2012-2015 Fidelity National Information		;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
-;									;
-;	This source code contains the intellectual property		;
-;	of its copyright holder(s), and is made available		;
-;	under a license.  If you do not know the terms of		;
-;	the license, please stop and do not read further.		;
-;									;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;								;
+; Copyright (c) 2018 YottaDB LLC. and/or its subsidiaries.	;
+; All rights reserved.						;
+;								;
+;	This source code contains the intellectual property	;
+;	of its copyright holder(s), and is made available	;
+;	under a license.  If you do not know the terms of	;
+;	the license, please stop and do not read further.	;
+;								;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Current ydbposix version.
 YDBPOSIXVERSION	;r3
