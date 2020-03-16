YottaDB/YDBPosix

commit 3b8abd6d4b55761b32209377615b82199215f518

better error handling for regmatch^%ydbposix

_ydbposix.m
@@ -134,9 +134,11 @@ regmatch(str,patt,pattflags,matchflags,matchresults,maxresults)
 	. if $increment(pfval,$select(nextpf'=+nextpf:$$regsymval(nextpf),1:nextpf))
 	else  set pfval=0
 	do:'$data(%ydbposix("regmatch",patt,pfval))
-	. if $&ydbposix.regcomp(.pregstr,patt,pfval,.errno)
-	. zkill %ydbposix("regcomp","errno")
-	. set %ydbposix("regmatch",patt,pfval)=pregstr
+	. do:'$&ydbposix.regcomp(.pregstr,patt,pfval,.errno)
+	. . zkill %ydbposix("regcomp","errno")
+	. . set %ydbposix("regmatch",patt,pfval)=pregstr
+	; nothing matched and that is due to an error in regcomp
+	i '$data(%ydbposix("regmatch",patt,pfval)) quit:$quit -errno_",regcomp" quit
 	set:'$data(maxresults) maxresults=1
 	set $zpiece(resultbuf,$zchar(0),maxresults*$$regsymval("sizeof(regmatch_t)")+1)=""
 	if $length($get(matchflags)) for i=1:1:$length(matchflags,"+") do
@@ -145,13 +147,16 @@ regmatch(str,patt,pattflags,matchflags,matchresults,maxresults)
 	else  set mfval=0
 	if $&ydbposix.regexec(%ydbposix("regmatch",patt,pfval),str,maxresults,.resultbuf,mfval,.matchsuccess)
 	zkill %ydbposix("regexec","errno")
+	s i=0
 	do:matchsuccess
 	. kill matchresults
 	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1 for i=1:1:maxresults do  if 'matchresults(i,"start") kill matchresults(i) quit
-	. . if $&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo)
-	. . set matchresults(i,"start")=1+nextrmso
-	. . set matchresults(i,"end")=1+nextrmeo
-	quit:$quit matchsuccess quit
+	. . k nextrmso,nextrmeo
+	. . if '$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo) do
+	. . . set matchresults(i,"start")=1+nextrmso
+	. . . set matchresults(i,"end")=1+nextrmeo
+	. i $increment(i,-1)
+	quit:$quit i quit
 
 ; Get numeric value for regular expression symbolic constant - only lower case because this is an internal utility routine
 regsymval(sym)

commit 9007dec8aa628e7ec64303ab5dfc5b26407746a2

Fix regression in $$regmatch^%ydbposix introduced in 3b8abd6
* The primary issue was a `if $increment(i,-1)` that was unconditionally done before the `quit:$quit`
  in `$$regmatch^%ydbposix`. I suspect the author of 3b8abd6 intended to do this in case
  the `$&ydbposix.regofft2offsets()` call returned a non-zero value (i.e. an error scenario)
  since that resulted in `matchresults(i,"start")` and `matchresults(i,"end")` not getting set.
  Therefore, this decrement is now moved to where it should have been by a `set i=i-1` just before
  we `quit` out of the for loop.

* A secondary issue in my opinion is that `if 'matchresults(i,"start")` done after the `do` call
  would result in an LVUNDEF error in case `matchresults(i,"start")` did not get set in the
  `do` body. This is now fixed by instead doing a `$data()` on it.

_ydbposix.m
@@ -3,7 +3,7 @@
 ; Copyright (c) 2012-2015 Fidelity National Information		;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
-; Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -150,12 +150,12 @@ regmatch(str,patt,pattflags,matchflags,matchresults,maxresults)
 	set i=0
 	do:matchsuccess
 	. kill matchresults
-	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1 for i=1:1:maxresults do  if 'matchresults(i,"start") kill matchresults(i) quit
+	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1
+	. for i=1:1:maxresults do  if '$data(matchresults(i,"start")) set i=i-1 quit
 	. . kill nextrmso,nextrmeo
-	. . if '$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo) do
+	. . do:'$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo)
 	. . . set matchresults(i,"start")=1+nextrmso
 	. . . set matchresults(i,"end")=1+nextrmeo
-	. if $increment(i,-1)
 	quit:$quit i quit
 
 ; Get numeric value for regular expression symbolic constant - only lower case because this is an internal utility routine