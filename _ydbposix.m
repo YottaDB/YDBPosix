YottaDB/YDBPosix

commit 9007dec8aa628e7ec64303ab5dfc5b26407746a2

Fix regression in $$regmatch^%ydbposix introduced in 3b8abd6
* The primary issue was a `if $increment(i,-1)` that was unconditionally done before the `quit:$quit`
  in `$$regmatch^%ydbposix`. I suspect the author of 3b8abd6 intended to do this in case
  the `$&ydbposix.regofft2offsets()` call returned a non-zero value (i.e. an error scenario)
  since that resulted in `matchresults(i,"start")` and `matchresults(i,"end")` not getting set.
  Therefore, this decrement is now moved to where it should have been by a `set i=i-1` just before
  we `quit` out of the for loop.

* A secondary issue in my opinion is that `if 'matchresults(i,"start")` done after the `do` call
  would result in an LVUNDEF error in case `matchresults(i,"start")` did not get set in the
  `do` body. This is now fixed by instead doing a `$data()` on it.

_ydbposix.m
@@ -3,7 +3,7 @@
 ; Copyright (c) 2012-2015 Fidelity National Information		;
 ; Services, Inc. and/or its subsidiaries. All rights reserved.	;
 ;								;
-; Copyright (c) 2018-2019 YottaDB LLC and/or its subsidiaries.	;
+; Copyright (c) 2018-2020 YottaDB LLC and/or its subsidiaries.	;
 ; All rights reserved.						;
 ;								;
 ;	This source code contains the intellectual property	;
@@ -150,12 +150,12 @@ regmatch(str,patt,pattflags,matchflags,matchresults,maxresults)
 	set i=0
 	do:matchsuccess
 	. kill matchresults
-	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1 for i=1:1:maxresults do  if 'matchresults(i,"start") kill matchresults(i) quit
+	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1
+	. for i=1:1:maxresults do  if '$data(matchresults(i,"start")) set i=i-1 quit
 	. . kill nextrmso,nextrmeo
-	. . if '$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo) do
+	. . do:'$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo)
 	. . . set matchresults(i,"start")=1+nextrmso
 	. . . set matchresults(i,"end")=1+nextrmeo
-	. if $increment(i,-1)
 	quit:$quit i quit
 
 ; Get numeric value for regular expression symbolic constant - only lower case because this is an internal utility routine

commit cbeff76f0612647093940382618b0c89349f1aec

Fix failure in regmatch^%ydbposixtest due to extraneous match results (regression in 9007dec)
The `v60001/gtm_posix` subtest in the YDBTest repo failed after commit 9007dec with the following diff.

> FAIL regmatch^%ydbposix 4
> FAIL REGMATCH^%ydbposix 4

This is because that commit erroneously assumed that `matchresults(i,"start")` might not be set in the
for loop in case `$&ydbposix.regofft2offsets` returned a non-zero value. And because of this, it added
a `$data(matchresults(i,"start"))` check instead of the pre-existing `'matchresults(i,"start"))` check.

Turns out `$&ydbposix.regofft2offsets` always returns a 0. Therefore, the `$data` check is not
necessary and in fact incorrect since we need to actually check for 0 value of `matchresults(i,"start")`
to terminate the for loop. This is now done. And comments added in the code to help a future reader.

_ydbposix.m
@@ -151,11 +151,15 @@ regmatch(str,patt,pattflags,matchflags,matchresults,maxresults)
 	do:matchsuccess
 	. kill matchresults
 	. set regmatchtsize=$$regsymval("sizeof(regmatch_t)"),j=1
-	. for i=1:1:maxresults do  if '$data(matchresults(i,"start")) set i=i-1 quit
+	. ; If we find `matchresults(i,"start")` is 0, it means match happened until `i-1` only hence the `kill` and `i-1` below
+	. for i=1:1:maxresults do  if 'matchresults(i,"start") kill matchresults(i) set i=i-1 quit
 	. . kill nextrmso,nextrmeo
-	. . do:'$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo)
+	. . ; Note: `$&ydbposix.regofft2offsets()` returns 0 in all cases where 3 parameters (valid # of parms) are passed.
+	. . ; Therefore, the `if` check always succeeds and the `do` code always executes.
+	. . if '$&ydbposix.regofft2offsets($zextract(resultbuf,j,$increment(j,regmatchtsize)-1),.nextrmso,.nextrmeo) do
 	. . . set matchresults(i,"start")=1+nextrmso
 	. . . set matchresults(i,"end")=1+nextrmeo
+	. . ; Because the `do` code above is always executed, `matchresults(i)` is guaranteed to be set at this point.
 	quit:$quit i quit
 
 ; Get numeric value for regular expression symbolic constant - only lower case because this is an internal utility routine